42ac9d08b8bcdd6c273ec63f494a03005fd2aef0
zhouzhou0921 <1017502250@qq.com>
2022-12-09 15:54:45 +0800

    v0.4

---
+++ R/add.R
@@ -0,0 +1,18 @@
+#' 加法
+#'
+#' @param x 参数x
+#' @param y 参数y
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' a =myadd()
+#' b =myadd(3,4)
+#' c =myadd(x=5,y=6)
+#' d =myadd(y=2,x=3)
+myadd <- function(x=1,y=2) {
+  res = x + y
+  return(res)
+
+}
